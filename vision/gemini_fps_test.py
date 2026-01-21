78fb74f24bf2a1951cc65e2e9752a6087ab07004
Qba <jfigiel@rscontrols.eu>
2026-01-21 12:48:25 +0100

    Detect Orbbec devices using Context

--- vision/gemini_fps_test.py
+++ vision/gemini_fps_test.py
@@ -1,1 +1,9 @@
-print("Gemini FPS test - start")
+from pyorbbecsdk import Context
+
+print("Creating Orbbec context...")
+
+ctx = Context()
+
+device_list = ctx.query_devices()
+
+print("Number of devices found:", device_list.get_count())
